2cde070add63b2c80adac83dc61345ad2725bf66
johanIbtaten <j.ibtaten@hotmail.fr>
2020-08-31 19:23:56 +0200

    add buttons modules

--- src/views/Board.vue
+++ src/views/Board.vue
@@ -1,78 +1,90 @@
 <template>
-  <div class="board">
-    <div class="flex flex-row items-start">
-      <!-- On parcourt les colonnes du trello -->
-      <BoardColumn
-        v-for="(column, $columnIndex) of board.columns"
-        :key="$columnIndex"
-        :column="column"
-        :columnIndex="$columnIndex"
-        :board="board"
-      />
-
-      <div class="column flex">
-        <input
-          type="text"
-          class="p-2 mr-2 flex-grow"
-          placeholder="New column name"
-          v-model="newColumnName"
-          @keyup.enter="createColumn"
-        >
-      </div>
+  <div>
+    <div class="other-mods">
+      <a
+        href="https://johanibtaten.github.io/vmod-texteditor-demo/"
+        target="_blank"
+        class="btn-mod"
+      >Demo Vue Module 1</a>
+      <a
+        href="https://johanibtaten.github.io/vmod-filesfolders-demo/"
+        target="_blank"
+        class="btn-mod"
+      >Demo Vue Module 2</a>
     </div>
+    <div class="board">
+      <div class="flex flex-row items-start">
+        <!-- On parcourt les colonnes du trello -->
+        <BoardColumn
+          v-for="(column, $columnIndex) of board.columns"
+          :key="$columnIndex"
+          :column="column"
+          :columnIndex="$columnIndex"
+          :board="board"
+        />
 
-    <div
-      class="task-bg"
-      v-if="isTaskOpen"
-      @mousedown.self="close"
-    >
-      <router-view @close-modal="close"/>
+        <div class="column flex">
+          <input
+            type="text"
+            class="p-2 mr-2 flex-grow"
+            placeholder="New column name"
+            v-model="newColumnName"
+            @keyup.enter="createColumn"
+          />
+        </div>
+      </div>
+
+      <div class="task-bg" v-if="isTaskOpen" @mousedown.self="close">
+        <router-view @close-modal="close" />
+      </div>
     </div>
   </div>
 </template>
 
 <script>
-import { mapState } from 'vuex'
-import BoardColumn from '@/components/BoardColumn'
+import { mapState } from "vuex";
+import BoardColumn from "@/components/BoardColumn";
 
 export default {
   components: { BoardColumn },
-  data () {
+  data() {
     return {
-      newColumnName: ''
-    }
+      newColumnName: "",
+    };
   },
   computed: {
     // On récupère l'objet board depuis le state. Il contient
     // toutes les données
-    ...mapState(['board']),
+    ...mapState(["board"]),
     // Si le nom de la route courante est task on renvoie true
     // la modal est alors affichée sinon c'est false
     // et la modal n'est pas affichée
-    isTaskOpen () {
-      return this.$route.name === 'task'
-    }
+    isTaskOpen() {
+      return this.$route.name === "task";
+    },
   },
   methods: {
-    close () {
-      this.$router.push({ name: 'board' })
+    close() {
+      this.$router.push({ name: "board" });
     },
-    createColumn () {
-      this.$store.commit('CREATE_COLUMN', {
-        name: this.newColumnName
-      })
-      this.newColumnName = ''
-    }
-  }
-}
+    createColumn() {
+      this.$store.commit("CREATE_COLUMN", {
+        name: this.newColumnName,
+      });
+      this.newColumnName = "";
+    },
+  },
+};
 </script>
 
 <style lang="css">
 .board {
   @apply p-4 bg-teal-dark h-full overflow-auto;
 }
-.task-bg {
-  @apply pin absolute;
-  background: rgba(0,0,0,0.5);
+.other-mods {
+  @apply py-5 bg-white flex justify-end;
+}
+.btn-mod {
+  @apply p-3 mr-5 rounded text-teal-dark border border-teal-dark border-2 no-underline;
 }
 </style>
